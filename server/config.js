348aa256f33f265c80e8f38219263b93a68de8ee
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2024-02-27 01:41:13 +0600

    0.3

--- server/config.js
+++ server/config.js
@@ -5,8 +5,10 @@ module.exports = {
   cors: {
     origin: [
       'http://localhost:3000', // Assuming this is your React app's origin
-      'http://localhost:8100', // Add your Capacitor app's origin here
+      'http://localhost:8100',
+      'http://localhost:8080', // Add your Capacitor app's origin here
       'http://127.0.0.1:8100',
+      'http://localhost',
       'capacitor://localhost',
       'ionic://localhost',
       'capacitor://localhost:8100',
