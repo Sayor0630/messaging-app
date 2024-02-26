1bde0b16eede1156454c802edc1d59ca4dcb319d
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2024-02-27 01:06:58 +0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,11 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   isDev,
   cors: {
-    origin: ['*'], // Allow requests from all origins
+    origin: [
+      'http://localhost:3000', // Assuming this is your React app's origin
+      'http://localhost:8100', // Add your Capacitor app's origin here
+      // Add more origins if needed
+    ],
   },
   db: {
     uri: process.env.MONGO_URI,
