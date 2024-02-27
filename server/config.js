0eaa988f1026951dff24c37e6951aaafd9ef86ce
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2024-02-27 11:55:43 +0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,18 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   isDev,
   cors: {
-    origin: [/^http:\/\/localhost/],
+    origin: [
+      'http://localhost:3000', // Assuming this is your React app's origin
+      'http://localhost:8100',
+      'http://localhost:8080', // Add your Capacitor app's origin here
+      'http://127.0.0.1:8100',
+      'http://localhost',
+      'capacitor://localhost',
+      'ionic://localhost',
+      'capacitor://localhost:8100',
+      'ionic://localhost:8100',
+      // Add more origins if needed
+    ],
   },
   db: {
     uri: process.env.MONGO_URI,
