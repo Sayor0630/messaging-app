5b988ecdd104e5a4052d170fa5e9a56d29662f7e
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2024-02-27 14:00:16 +0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,19 +3,7 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   isDev,
   cors: {
-    origin: [
-      'http://localhost:3000', // Assuming this is your React app's origin
-      'http://localhost:8100',
-      'http://localhost:8080', // Add your Capacitor app's origin here
-      'http://127.0.0.1:8100',
-      'http://localhost',
-      'capacitor://localhost',
-      'ionic://localhost',
-      'capacitor://localhost:8100',
-      'ionic://localhost:8100',
-      'https://kotha-jet.vercel.app'
-      // Add more origins if needed
-    ],
+    origin: '*',
   },
   db: {
     uri: process.env.MONGO_URI,
